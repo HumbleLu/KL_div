3c79c4991151231fa91a479a994984ba52916345
chienlu <chienlu@dsl-trebrasgw1-54fa40-40.dhcp.inet.fi>
2017-01-12 13:19:22 +0200

    dde

--- DDE.R
+++ DDE.R
@@ -7,7 +7,7 @@ G_mtx<- cmpfun(
   function(X, sigma){
     d<- ncol(X)
     dist_mtx<- as.matrix(dist(X))
-    (pi * sigma^2)^(d/2) * exp(-dist_mtx^2/4*sigma^2)
+    (pi * sigma^2)^(d/2) * exp(dist_mtx^2/(-4*sigma^2))
   }
 )
 
@@ -24,14 +24,16 @@ psi_func<- function(X, sigma){
   )
 }
 
-## psi_j: here the trace of Hessian matrix
-psi2_func<- function(X, j, sigma){
+## phi_j: here only implement the trace of Hessian matrix
+phi_func<- function(X, j, sigma){
+  ## jth element in the diagonal of Hessian matrix
+  
   cmpfun(
     function(x){
       if(class(x) == "matrix"){
-        psi_func(X, sigma)(x) * (dist2(x[, j, drop = F], X[, j, drop = F])^2 - sigma^2)/(-sigma^4)
+        psi_func(X, sigma)(x) * (dist2(x[, j, drop = F], X[, j, drop = F])^2 - sigma^2)/(sigma^4)
       }else{
-        psi_func(X, sigma)(x) * ((x[j] - X[,j])^2 - sigma^2)/(-sigma^4)
+        psi_func(X, sigma)(x) * ((x[j] - X[,j])^2 - sigma^2)/(sigma^4)
       }
     }
   )
@@ -46,31 +48,31 @@ psi2_func<- function(X, j, sigma){
 # psi_func(yy, 1)(xx)
 # psi2_func(yy, 1, 1)(xx)
 
-
 ## Second derivative estimation
 theta<- cmpfun(
   function(X, j, sigma, lambda){
     d<- ncol(X)
-    n<- nrow(X)
+    b<- nrow(X)
     
     # constructing h_j
-    mtx<- matrix(rep(X[, j, drop = F], n), ncol = n)
-    dist_mtx<- as.matrix(dist(X))
-    h<- apply(exp(dist_mtx^2/-2*sigma^2)/-4*sigma^2 * ((mtx - t(mtx))^2 - sigma^2), 2, mean)
+    # mtx<- matrix(rep(X[, j, drop = F], n), ncol = n)
+    # dist_mtx<- as.matrix(dist(X))
+    # h<- apply(exp(dist_mtx^2/-2*sigma^2)/-4*sigma^2 * ((mtx - t(mtx))^2 - sigma^2), 2, mean)
+    h<- apply(phi_func(X, j, sigma)(X), 1, mean)
     
     # get theta_j
-    solve(G_mtx(X, sigma) + lambda * diag(n)) %*% h
+    solve(G_mtx(X, sigma) + lambda * diag(b)) %*% h
   }
 )
 
 g_func<- function(X, j, sigma, lambda, theta_j = NULL){
   # get theta_j
-  if(is.null(theta_j)){
-    theta_j<- theta(X, j, sigma, lambda)
-  }
+
   cmpfun(
     function(x){
       psi<- psi_func(X, sigma)(x)
+      
+      theta_j<- theta(X, j, sigma, lambda)
       
       if(class(psi) == "matrix"){
         t(theta_j) %*% t(psi)
@@ -84,26 +86,25 @@ g_func<- function(X, j, sigma, lambda, theta_j = NULL){
 # corss validation (hold-out)
 # TODO 
 # CV
+
 CV_t<- function(X_t, x_t, j, sigma, lambda){
   d<- ncol(X_t)
-  n<- nrow(X_t)
+
+  # theta_hat<- theta(X_t, j, sigma, lambda)
+  theta_hat<- theta(x_t, j, sigma, lambda)
   
-  theta_hat<- theta(X_t, j, sigma, lambda)
-  theta_prod<- tcrossprod(theta_hat)
-  
-  est_func<- g_func(X_t, j, sigma, lambda, theta_hat)
-  
-  (sqrt(pi)*sigma)^d * sum(theta_hat^2) +
-    sum(2 * (sqrt(pi)*sigma)^d * theta_prod[lower.tri(theta_prod)] * exp(as.vector(dist(X_t))/(-4*sigma^2))) -
-    (2/n) * sum(est_func(x_t))
+  # t(theta_hat) %*% G_mtx(X_t, sigma) %*% theta_hat - (2/n) * sum(g_func(X_t, j, sigma, lambda, theta_hat)(x_t))
+    
+  t(theta_hat) %*% G_mtx(x_t, sigma) %*% theta_hat - 2 * mean(t(theta_hat) %*% t(phi_func(x_t, j, sigma)(X_t)))
+  # t(theta_hat) %*% G_mtx(X_t, sigma) %*% theta_hat - 2 * mean(t(theta_hat) %*% t(phi_func(X_t, j, sigma)(x_t)))
 }
+
 
 CV<- function(X, j, sigma, lambda, t){
   # t: number of partitions
-  
   n<- nrow(X)
   start<- seq(1, n, n/t)
-  end<- seq(5, n, n/t)
+  end<- start + n/t - 1
   
   mean(
     mapply(function(s, e) CV_t(X[-c(s:e),,drop = F], X[s:e,,drop = F], j, sigma, lambda),
@@ -111,3 +112,4 @@ CV<- function(X, j, sigma, lambda, t){
            e = end)
   )
 }
+
